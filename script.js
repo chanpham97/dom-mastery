f76f8fab3a9927c91cb4fa7a0e630b83ea6c4f6d
Chan Pham <iamchanpham@gmail.com>
2025-05-01 16:39:55 +0000

    mastery template

---
+++ script.js
@@ -0,0 +1,10 @@
+// Select the body here & console log it to check
+
+// Select the first p tag here & console log it to check
+
+// Select all the important-text p tags here & console log it to check
+
+// Select the first button & console log it to check
+
+// Select the second button & console log it to check
+
